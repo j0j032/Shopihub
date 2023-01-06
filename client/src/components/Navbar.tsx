d6cb719f2c34f124c81ef8885af6e011f5e21f3f
Jordan <100964858+j0j032@users.noreply.github.com>
2023-01-06 22:38:40 +0100

    Client: - configure router - Set up Layout and dashboard components - ✨ introduce FlexBetween styled component - ✨ introduce Navbar component - update tsconfig to match Mui

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import {useState} from "react";
+import {
+    LightModeOutlined,
+    DarkModeOutlined,
+    Menu as MenuIcon,
+    Search,
+    SettingsOutlined,
+    ArrowDropDownOutlined
+} from "@mui/icons-material";
+import FlexBetween from "./FlexBetween";
+import {useDispatch} from "react-redux";
+import {setMode} from "../state";
+import profilePic from 'assets/profile.jpg'
+import {AppBar, IconButton, InputBase, Toolbar, useTheme} from "@mui/material";
+
+type NavbarProps = {}
+
+export function Navbar({}: NavbarProps) {
+    const dispatch = useDispatch()
+    const theme = useTheme()
+
+    return <AppBar sx={{position: 'static', background: 'none', boxShadow: 'none'}}>
+        <Toolbar sx={{justifyContent: 'space-between'}}>
+            <FlexBetween>
+                <IconButton onClick={() => console.log('open/close sideBar')}>
+                    <MenuIcon/>
+                </IconButton>
+                <FlexBetween backgroundColor={theme.palette.background.paper} borderRadius='9px' gap='3rem'
+                             p='0.1rem 1.5rem'>
+                    <InputBase placeholder='Search..'/>
+                    <IconButton>
+                        <Search/>
+                    </IconButton>
+                </FlexBetween>
+            </FlexBetween>
+
+            <FlexBetween gap='1.5rem'>
+                <IconButton onClick={() => dispatch(setMode())}>
+                    {theme.palette.mode === 'dark'
+                        ? <DarkModeOutlined sx={{fontSize: '25px'}}/>
+                        : <LightModeOutlined sx={{fontSize: '25px'}}/>}
+                </IconButton>
+                <IconButton>
+                    <SettingsOutlined sx={{fontSize: '25px'}}/>
+                </IconButton>
+            </FlexBetween>
+        </Toolbar>
+    </AppBar>
+}
