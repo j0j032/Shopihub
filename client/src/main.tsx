cc0d39a19d6f5f84b5e027ea6ca72e7943a114bf
Jordan <100964858+j0j032@users.noreply.github.com>
2023-01-07 01:34:30 +0100

    Client: Setup RTK Query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,12 +5,17 @@ import './index.css'
 import {configureStore} from "@reduxjs/toolkit";
 import globalReducer from "./state";
 import {Provider} from "react-redux";
+import {setupListeners} from "@reduxjs/toolkit/query";
+import {api} from './state/api'
 
 const store = configureStore({
     reducer: {
         global: globalReducer,
+        [api.reducerPath]: api.reducer,
     },
+    middleware: (getDefault) => getDefault().concat(api.middleware)
 });
+setupListeners(store.dispatch)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
