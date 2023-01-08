2141c3f512745ec2d6f6d6fb3667137aa33f3fbf
Jordan <100964858+j0j032@users.noreply.github.com>
2023-01-08 10:43:25 +0100

    🔨 | Client | Get user | create api/user & getMethods |

---
+++ client/src/api/user.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import {useQuery} from "react-query";
+import User from "../types/User";
+
+const BASE_URL: string = import.meta.env.VITE_REACT_APP_BASE_URL
+
+const getUser = (userId: string) => axios.get<User>(`${BASE_URL}/general/user/${userId}`).then(res => res.data)
+
+export const useGetUser = (useriId: string) => {
+    return useQuery(['user'], () => getUser(useriId), {
+        refetchOnWindowFocus: false
+    })
+}
