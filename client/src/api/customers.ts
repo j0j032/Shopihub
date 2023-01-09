c01264ccdd45ff087268c42863c2a86d2f5a0917
Jordan <100964858+j0j032@users.noreply.github.com>
2023-01-09 11:36:39 +0100

    ✨ | Client | Customers | Create Customers page |

---
+++ client/src/api/customers.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import {useQuery} from "react-query";
+import User from "../types/User";
+
+const BASE_URL: string = import.meta.env.VITE_REACT_APP_BASE_URL
+
+const getCustomers = () => axios.get<User[]>(`${BASE_URL}/client/customers`).then(r => r.data)
+
+export const useGetCustomers = () => {
+    return useQuery(['customers'], () => getCustomers(), {
+        refetchOnWindowFocus: false
+    })
+}
---
+++ client/src/pages/Customers.tsx
@@ -0,0 +1,95 @@
+import {Box, useTheme} from "@mui/material";
+import {GridCellParams} from '@mui/x-data-grid';
+import {Header} from "../components/Header";
+import {DataGrid} from "@mui/x-data-grid";
+import {PaletteColorKey} from "../../mui";
+import {useGetCustomers} from "../api/customers";
+
+type CustomersProps = {}
+
+export function Customers({}: CustomersProps) {
+    const theme = useTheme();
+    const {data, isLoading} = useGetCustomers()
+
+    const columns = [
+        {
+            field: "_id",
+            headerName: "ID",
+            flex: 1,
+        },
+        {
+            field: "name",
+            headerName: "Name",
+            flex: 0.5,
+        },
+        {
+            field: "email",
+            headerName: "Email",
+            flex: 1,
+        },
+        {
+            field: "phoneNumber",
+            headerName: "Phone Number",
+            flex: 0.5,
+            renderCell: (params: GridCellParams) => {
+                return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3");
+            },
+        },
+        {
+            field: "country",
+            headerName: "Country",
+            flex: 0.4,
+        },
+        {
+            field: "occupation",
+            headerName: "Occupation",
+            flex: 1,
+        },
+        {
+            field: "role",
+            headerName: "Role",
+            flex: 0.5,
+        },
+    ];
+
+    return (
+        <Box padding='4.2rem 2.5rem 1.5rem 2.5rem'>
+            <Header title='CUSTOMERS' subtitle='List of Customers'/>
+            <Box
+                mt='40px'
+                height='75vh'
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none",
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: theme.palette.background.paper,
+                        color: theme.palette.secondary[100 as PaletteColorKey],
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: theme.palette.primary.light,
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        backgroundColor: theme.palette.background.paper,
+                        color: theme.palette.secondary[100 as PaletteColorKey],
+                        borderTop: "none",
+                    },
+                    "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                        color: `${theme.palette.secondary[200 as PaletteColorKey]} !important`,
+                    },
+                }}
+            >
+                <DataGrid
+                    loading={isLoading || !data}
+                    getRowId={(row) => row._id}
+                    rows={data || []}
+                    columns={columns}
+                />
+            </Box>
+        </Box>
+    )
+}
